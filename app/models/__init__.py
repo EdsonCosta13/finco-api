f16c38e37389fc488c21f5ed14cfad4cb046396f
EdsonCosta13 <edsonpaulojoaodacosta@gmail.com>
2025-05-27 10:39:16 +0100

    feat: add wallet and payment models to initialization
    - Included Wallet, WalletTransaction, and Payment models in the app initialization file.
    - Enhanced the structure of the models to support wallet management and payment processing functionalities.

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -4,3 +4,6 @@ from app.models.employee import Employee
 from app.models.credit_request import CreditRequest, CreditRequestStatus
 from app.models.investment import Investment
 from app.models.invitation import CompanyInvitation, EmployeeInvitation
+from app.models.wallet import Wallet
+from app.models.wallet_transaction import WalletTransaction
+from app.models.payment import Payment, PaymentStatus, PaymentType
